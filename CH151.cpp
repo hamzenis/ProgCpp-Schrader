d0a57956ee27501f1f3341581b192796b52fe26c
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-04-25 18:27:18 +0200

    finale Version

---
+++ CH151.cpp
@@ -0,0 +1,28 @@
+/*
+ * File: CH151.cpp
+ * Project: Einführung in C - Schrader
+ * Created Date: 2022-04-25, 10:29:07 am
+ * Author: Hamzenis Kryeziu
+ * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
+ * -----
+ * Last Modified: 2022-04-25, 10:36:58 am
+ * Modified By: Hamzenis Kryeziu
+ * -----
+ * Copyright (c) 2022
+ *
+ * Free for use
+ * -----
+ * HISTORY:
+ * Date              		By		Comments
+ * ------------------		----	----------------------------------------------------------
+ * 2022-04-25, 10:29:07 am	H.K.	-start-
+*/
+
+#include <iostream>
+
+int main() {
+    
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
