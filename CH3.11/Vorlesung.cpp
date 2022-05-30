0201a59cfdea81905dd8a3a6eb09034cd87b1f02
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-05-30 13:40:26 +0200

    CH3.11

---
+++ CH3.11/Vorlesung.cpp
@@ -0,0 +1,81 @@
+/*
+ * File: Vorlesung.cpp
+ * Project: Objektorientierte Progammierung in C++ - Schrader
+ * Created Date: 2022-05-30, 10:56:42 am
+ * Author: Hamzenis Kryeziu
+ * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
+ * -----
+ * Last Modified: 2022-05-30, 11:22:27 am
+ * Modified By: Hamzenis Kryeziu
+ * -----
+ * Copyright (c) 2022
+ *
+ * Free for use
+ * -----
+ * HISTORY:
+ * Date              		By		Comments
+ * ------------------		----	----------------------------------------------------------
+ * 2022-05-30, 10:56:42 am	H.K.	-start-
+ */
+
+#include "Vorlesung.h"
+#include <iostream>
+
+ //  Constructor
+Vorlesung::Vorlesung(int id,
+    std::string name,
+    std::string dozent,
+    int ect,
+    int sws) {
+
+    this->name = name;
+    this->dozent = dozent;
+    this->ect = ect;
+    this->sws = sws;
+}
+
+//  Getter
+int Vorlesung::getId() {
+    return id;
+}
+std::string Vorlesung::getName() {
+    return name;
+}
+std::string Vorlesung::getDozent() {
+    return dozent;
+}
+int Vorlesung::getEct() {
+    return ect;
+}
+int Vorlesung::getSws() {
+    return sws;
+}
+
+//  Setter
+void Vorlesung::setId(int id) {
+    this->id = id;
+}
+void Vorlesung::setName(std::string name) {
+    this->name = name;
+}
+void Vorlesung::setDozent(std::string dozent) {
+    this->dozent = dozent;
+}
+void Vorlesung::setEct(int ect) {
+    this->ect = ect;
+}
+void Vorlesung::setSws(int sws) {
+    this->sws = sws;
+}
+
+//  Funktionen
+void Vorlesung::printObject() {
+    std::cout   << id
+                << " "
+                << name + " "
+                << dozent + " "
+                << ect
+                << " "
+                << sws
+                << std::endl;
+}
---
+++ CH3.11/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * File: main.cpp
+ * Project: Objektorientierte Progammierung in C++ - Schrader
+ * Created Date: 2022-05-30, 11:25:50 am
+ * Author: Hamzenis Kryeziu
+ * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
+ * -----
+ * Last Modified: 2022-05-30, 11:26:59 am
+ * Modified By: Hamzenis Kryeziu
+ * -----
+ * Copyright (c) 2022
+ *
+ * Free for use
+ * -----
+ * HISTORY:
+ * Date              		By		Comments
+ * ------------------		----	----------------------------------------------------------
+ * 2022-05-30, 11:25:50 am	H.K.	-start-
+ */
+
+#include <iostream>
+#include <Vorlesung.cpp>
+
+int main() {
+    
+    return 0;
+}
+
