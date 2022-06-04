5be8910dcfd39b15e00b1000ca2365c71112a1e4
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-06-04 23:52:15 +0200

    Update StudentCourseExam1.cpp

--- CH5.6/StudentCourseExam1.cpp
+++ CH5.6/StudentCourseExam1.cpp
@@ -1,5 +1,22 @@
-// StudentCourseExam1.cpp : program with student, course, exams classes
-//
+/*
+ * File: StudentCourseExam1.cpp
+ * Project: Objektorientierte Progammierung in C++ - Schrader
+ * Created Date: 2022-06-03, 12:13:19 am
+ * Author: Hamzenis Kryeziu
+ * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
+ * -----
+ * Last Modified: 2022-06-04, 11:48:45 pm
+ * Modified By: Hamzenis Kryeziu
+ * -----
+ * Copyright (c) 2022 
+ * 
+ * Free for use
+ * -----
+ * HISTORY:
+ * Date              		By		Comments
+ * ------------------		----	----------------------------------------------------------
+ * 2022-06-03, 12:13:19 am	H.K.	-start-
+ */
 
 #include <iostream>
 #include <iomanip>
@@ -8,42 +25,11 @@
 
 using namespace std;
 
-//  converts the grades to strings in german grade notation
-string convertToGrade(float inGrade){
-    string outGrade;
-    if(inGrade >= 1 &&  inGrade < 1.2)   outGrade = "1+";
-    if(inGrade >= 1.2 &&  inGrade < 1.6) outGrade = "1";
-    if(inGrade >= 1.6 &&  inGrade < 1.9) outGrade = "1-";
-    if(inGrade >= 1.9 &&  inGrade < 2.2) outGrade = "2+";
-    if(inGrade >= 2.2 &&  inGrade < 2.6) outGrade = "2";
-    if(inGrade >= 2.6 &&  inGrade < 2.9) outGrade = "2-";
-    if(inGrade >= 2.9 &&  inGrade < 3.2) outGrade = "3+";
-    if(inGrade >= 3.2 &&  inGrade < 3.6) outGrade = "3";
-    if(inGrade >= 3.6 &&  inGrade < 3.9) outGrade = "3-";
-    if(inGrade >= 3.9 &&  inGrade < 4.2) outGrade = "4+";
-    if(inGrade >= 4.2 &&  inGrade < 4.6) outGrade = "4";
-    if(inGrade >= 4.6 &&  inGrade < 4.9) outGrade = "4-";
-    if(inGrade >= 4.9 &&  inGrade < 5.2) outGrade = "5+";
-    if(inGrade >= 5.2 &&  inGrade < 5.6) outGrade = "5";
-    if(inGrade >= 5.6 &&  inGrade < 5.9) outGrade = "5-";
-    if(inGrade >= 5.9 &&  inGrade <= 6)  outGrade = "6";
-    return outGrade;
-}
-//  the last print
-void printEnd(float GPA, string studentShortName){
-        cout    << "The GPA for " 
-            << studentShortName
-            << " is " 
-            << convertToGrade(GPA) 
-            << " Decimal: " 
-            << setprecision(3) << GPA << endl;
-}
-
 //  initialize vectors
 vector<Student> students = { Student(1,"George", "Burdell"),
                             Student(2,"Nancy", "Rhodes") };
 
-vector<Course> courses = {  Course(1,"Algebra",5),
+vector<Course> courses = { Course(1,"Algebra",5),
                             Course(2,"Physics",4),
                             Course(3,"English",3),
                             Course(4,"Economics",4) };
@@ -59,35 +45,55 @@ vector<Grade> grades = { Grade(1,1,2),
 float GPA = 0.0f;
 int id;
 
-int main() {
-    string studentName;      // long name of student
-    string studentShortName; // short name of student
+//  converts the grades to strings in german grade notation
+string convertToGrade(float inGrade) {
+    string outGrade;
+    if (inGrade >= 1 && inGrade < 1.2)   outGrade = "1+";
+    if (inGrade >= 1.2 && inGrade < 1.6) outGrade = "1";
+    if (inGrade >= 1.6 && inGrade < 1.9) outGrade = "1-";
+    if (inGrade >= 1.9 && inGrade < 2.2) outGrade = "2+";
+    if (inGrade >= 2.2 && inGrade < 2.6) outGrade = "2";
+    if (inGrade >= 2.6 && inGrade < 2.9) outGrade = "2-";
+    if (inGrade >= 2.9 && inGrade < 3.2) outGrade = "3+";
+    if (inGrade >= 3.2 && inGrade < 3.6) outGrade = "3";
+    if (inGrade >= 3.6 && inGrade < 3.9) outGrade = "3-";
+    if (inGrade >= 3.9 && inGrade < 4.2) outGrade = "4+";
+    if (inGrade >= 4.2 && inGrade < 4.6) outGrade = "4";
+    if (inGrade >= 4.6 && inGrade < 4.9) outGrade = "4-";
+    if (inGrade >= 4.9 && inGrade < 5.2) outGrade = "5+";
+    if (inGrade >= 5.2 && inGrade < 5.6) outGrade = "5";
+    if (inGrade >= 5.6 && inGrade < 5.9) outGrade = "5-";
+    if (inGrade >= 5.9 && inGrade <= 6)  outGrade = "6";
+    return outGrade;
+}
+//  the last print
+void printEnd(string studentShortName) {
+    cout << "The GPA for "
+        << studentShortName
+        << " is "
+        << convertToGrade(GPA)
+        << " Decimal: "
+        << setprecision(3) << GPA << endl;
+}
 
-    //  select a student
-    cout << "Enter a student ID: ";
-    cin >> id;
+//  prints the individual grades
+void printIndividual(int j, string studentShortName, Grade grd) {
+    cout << studentShortName << ": "
+        << courses[j].getName() << " - "
+        << courses[j].getCredits() << " - "
+        << grd.getGrade() << " - "
+        << convertToGrade(grd.getGrade()) << endl;
+}
 
-    //  find name of student
-    int i = 0;
-    while (i < students.size() && students[i].getID() != id)
-        i++;
-
-    if (i == students.size()) {
-        cout << "Student not found!" << endl;
-        exit(1);
-    }
-    studentName = students[i].getName();
-    studentShortName = students[i].getShortName();
-
-
-    cout << "Results for: " << studentName << endl;
-
-    //  calculation and printing
+//  calculate and print the GPA
+float calcprint(string studentShortName, string studentName, int id) {
+    float ergebnis = 0.0f;
     float points = 0.0f;
     float credits = 0.0f;
+
+    cout << "Results for: " << studentName << endl;
     for (Grade& grd : grades)
         if (grd.getStudentID() == id) {
-
             //  calculate total credits and points
             int j = 0;
             while (j < courses.size() && courses[j].getID() != grd.getCourseID())
@@ -95,16 +101,48 @@ int main() {
             credits += courses[j].getCredits();
             points += grd.getGrade() * courses[j].getCredits();
 
-            //  prints the individual grades
-            cout << studentShortName << ": "
-                << courses[j].getName() << " - "
-                << courses[j].getCredits() << " - "
-                << grd.getGrade() << " - "
-                << convertToGrade(grd.getGrade()) << endl;
-        }
-    GPA = points / credits;
+            printIndividual(j, studentShortName, grd);
+        };
+    ergebnis = points / credits;
 
-    printEnd(GPA, studentShortName);
+    return ergebnis;
+}
+
+//  find name of student
+int findStudent() {
+    int i = 0;
+    while (i < students.size() && students[i].getID() != id)
+        i++;
+    return i;
+}
+
+//  select a student
+void selectStudent() {
+    cout << "Enter a student ID: ";
+    cin >> id;
+}
+
+//  checks if student exists
+void notFoundCheck(int i) {
+    if (i == students.size()) {
+        cout << "Student not found!" << endl;
+        exit(1);
+    }
+}
+
+int main() {
+    string studentName;
+    string studentShortName;
+
+    selectStudent();
+    int i = findStudent();
+    notFoundCheck(i);
+
+    studentName = students[i].getName();
+    studentShortName = students[i].getShortName();
+
+    GPA = calcprint(studentShortName, studentName, id);
+    printEnd(studentShortName);
 
     return (0);
 }
