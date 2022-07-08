ae3e0d306735c9203ff31df856c7a3d896bd54b2
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-07-08 21:04:34 +0200

    Update CH9.4.cpp

--- CH9.4.cpp
+++ CH9.4.cpp
@@ -1,3 +1,23 @@
+/*
+ * File: CH9.4.cpp
+ * Project: Objektorientierte Progammierung in C++ - Schrader
+ * Created Date: 2022-07-08, 6:27:12 pm
+ * Author: Julian Siefert
+ * E-Mail:
+ * -----
+ * Last Modified: 2022-07-08, 9:04:18 pm
+ * Modified By: Hamzenis Kryeziu
+ * -----
+ * Copyright (c) 2022 
+ * 
+ * Free for use
+ * -----
+ * HISTORY:
+ * Date              		By		Comments
+ * ------------------		----	----------------------------------------------------------
+ */
+
+
 #include <iostream>
 using namespace std;
 class person{
